c09d0d7ebba0b9acfa7d14d34c1b9b985ec0952f
Viktor Kindrat <kindratvictor07@gmail.com>
2023-06-28 11:48:15 +0300

    Home page style change

--- src/Components/App/Home/Home.jsx
+++ src/Components/App/Home/Home.jsx
@@ -1,20 +1,19 @@
-import bg from "./images/bg.png"
+import bg from "./Images/background.webp"
 
 import { Grid, Typography } from "@mui/material"
 
 function Home(){
     return (
-        <section className="Home">
-            <Grid sx={{minHeight: "calc(100vh - 150px)"}} container justifyContent="center" alignItems="center" direction="column" spacing={2}>
-                <Grid item>
-                    <Grid container justifyContent="center" alignItems="center">
-                        <img style={{width: "min(350px, 100%)", objectFit: "cover", height: "auto"}} width={350} src={bg} alt="Rick and Morty" />
-                    </Grid>
-                </Grid>
+        <section className="Home" style={{ marginBottom:"75px", height: "100vh", background: `linear-gradient(#00000050, #00000050), url("${bg}") center no-repeat`, backgroundSize: "cover, cover"}}>
+            <Grid p={"25px"} sx={{minHeight: "calc(100vh - 150px)"}} container justifyContent="center" alignItems="flex-start" direction="column" spacing={2}>
                 <Grid>
-                    <Typography variant="h5">
-                        <h1 className="Home__headline">Rick&Morty fan page</h1>
+                    <Typography variant="h1" fontWeight={900}>
+                        Rick and Morty fan page
                     </Typography>
+                    <Typography variant="subtitle1">
+                        Explore the world of Rick and Morty series
+                    </Typography>
+
                 </Grid>
             </Grid>
         </section>
